1068b2d8dce2c1b1b3be0971b31243639a40b433
wiolag123 <103309140+wiolag123@users.noreply.github.com>
2022-04-10 09:47:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Testowanie jest super SUPER!!!!!!!!!!!!!!');
+  res.end('Testowanie jest super SUPER w niedzielę!!!!!!!!!!!!!!');
 }
 const server = http.createServer(requestListener);
 server.listen(port);
